56c913fc7604930fdde1bd1d5981dd08a10c40f8
Hubert Młodzianowski <hubert.mlodzianowski@sollers.eu>
2021-07-11 18:55:48 +0200

    libraries update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject craftofwar "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :min-lein-version "2.9.0"
+  :min-lein-version "2.9.5"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -8,10 +8,10 @@
   :ring {:handler craftofwar.core/handler
           :auto-reload? true
           :auto-refresh? true}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.8.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [ring "1.9.3"]
                  [hiccup "1.0.5"]
-                 [compojure "1.5.1"]
+                 [compojure "1.6.2"]
                  [environ "1.2.0"]
                  [medley "1.3.0"]]
   :main ^:skip-aot craftofwar.core
